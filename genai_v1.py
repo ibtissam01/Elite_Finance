81b986205dbeb537b6de7b6ea11f94a47332f517
Ibtissam Labyady <89752387+ibtissam01@users.noreply.github.com>
2023-12-17 14:53:45 +0100

    Update genai_v1.py

--- genai_v1.py
+++ genai_v1.py
@@ -11,8 +11,6 @@ Original file is located at
 import streamlit as st
 import pandas as pd
 from langchain.chat_models import ChatOpenAI
-from langchain.agents import create_pandas_dataframe_agent
-from langchain.agents.agent_types import AgentType
 
 # Page title
 st.set_page_config(page_title='🦜🔗 Elite Finance')
